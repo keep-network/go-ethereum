c2bdb96774ac4d74527cf4b472e7a5238d93116f
Piotr Dyraga <piotr.dyraga@akena.engineering>
2019-06-25 20:40:09 +0200

    Reordered imports
    No changes in what modules are imported. I've just removed the empty
    line and auto-formatted imports.

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -22,13 +22,12 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/keep-network/blake2"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/crypto/bn256"
 	"github.com/ethereum/go-ethereum/params"
+	"github.com/keep-network/blake2"
 	"golang.org/x/crypto/ripemd160"
 )
 
